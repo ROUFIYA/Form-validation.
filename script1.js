fe2b20dd54d3bcdef0bf8604261ea070e3632b75
ROUFIYA <135303245+ROUFIYA@users.noreply.github.com>
2024-08-29 11:48:40 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,63 @@
+const form = document.getElementById('myForm');
+const errorMessages = document.getElementById('errorMessages');
+const errorMessages1 = document.getElementById('errorMessages1');
+const errorMessages2 = document.getElementById('errorMessages2');
+const errorMessages3 = document.getElementById('errorMessages3');
+const errorMessages4 = document.getElementById('errorMessages4');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const phone = document.getElementById('phone').value.trim();
+    const password = document.getElementById('password').value.trim();
+    const confirmPassword = document.getElementById('confirmPassword').value.trim();
+
+  
+
+    if (name === '' || name <= 5) {
+        document.getElementById('errorMessages').innerHTML='Please enter your name atleast 5 character';
+    }
+
+    if (email === '') {
+        document.getElementById('errorMessages1').innerHTML='Please enter your email address';
+    } else if (!validateEmail(email)) {
+        document.getElementById('errorMessages1').innerHTML='Please enter a valid email address';
+    }
+
+    if (phone === '') {
+        document.getElementById('errorMessages2').innerHTML='Please enter your phone number';
+    } else if (!validatePhone(phone)) {
+        document.getElementById('errorMessages2').innerHTML='Please enter a valid phone number';
+    }
+
+    if (password === '') {
+        document.getElementById('errorMessages3').innerHTML='Please enter your password';
+    } else if (password.length < 8) {
+       document.getElementById('errorMessages3').innerHTML='Password must be at least 8 characters long';
+    }
+
+    if (confirmPassword === '') {
+        document.getElementById('errorMessages4').innerHTML='Please confirm your password';
+    } else if (confirmPassword !== password) {
+        document.getElementById('errorMessages4').innerHTML='Passwords do not match';
+    }
+
+    if (errors.length > 0) {
+        errorMessages.innerText = errors.join('\n');
+    } else {
+        // Form is valid, you can submit it to the server
+        console.log('Form is valid');
+    }
+});
+
+function validateEmail(email) {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+}
+
+function validatePhone(phone) {
+    const phoneRegex = /^\d{3}-\d{3}-\d{4}$/;
+    return phoneRegex.test(phone);
+}
